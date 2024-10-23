7bd68a44bac889d25aa93cde472a31a96529c068
starry-skies <mmazin2028@kehillahstudent.org>
2024-10-23 10:43:00 -0700

    finished square

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 import turtle
-
-# YOUR CODE HERE
+def draw_square(size):
+    for i in range(4):
+        turtle.forward(size)
+        turtle.right(90)
+draw_square(100)
+turtle.forward(50)
+turtle.right(45)
+draw_square(71)
 
 turtle.exitonclick()
